00be61be8d340209554d7f485198f18fa835d4f1
Lazuta <lenya.lazuta@mail.ru>
2021-05-29 16:03:56 +0300

    JavaScript libarary from progect

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,8 @@
 export class DomListener {
-
+    constructor($root) {
+        if (!$root) {
+            throw new Error('[DomListener] no $root provided');
+        }
+        this.$root = $root;
+    }
 }
