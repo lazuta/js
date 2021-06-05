18bae350fbba301f724721c081cc80663a3d4f25
lazuta <leonid.lazuta@mail.ru>
2021-06-05 13:43:59 +0300

    Holliday commit 2 =)

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,34 +1,34 @@
-import {ExcelComponent} from '@core/ExcelComponent';
-import { createTable } from './table.template';
+// import {ExcelComponent} from '@core/ExcelComponent';
+// import { createTable } from './table.template';
 
 
-export class Table extends ExcelComponent {
-    static className = 'excel__table';
+// export class Table extends ExcelComponent {
+//     static className = 'excel__table';
 
-    constructor($root) {
-        super($root, {
-            name: 'Table',
-            listeners: ['click', 'mousedown', 'mouseup', 'mousemove']
-        });
-    }
+//     constructor($root) {
+//         super($root, {
+//             name: 'Table',
+//             listeners: ['click', 'mousedown', 'mouseup', 'mousemove']
+//         });
+//     }
 
-    onClick(event) {
-        // console.log(event.target)
-    }
+//     onClick(event) {
+//         // console.log(event.target)
+//     }
 
-    onMouseup(event) {
-        // console.log(event.target)
-    }
+//     onMouseup(event) {
+//         // console.log(event.target)
+//     }
 
-    onMousedown(event) {
-        // console.log(event.target)
-    }
+//     onMousedown(event) {
+//         // console.log(event.target)
+//     }
 
-    onMousemove(event) {
-        // console.log(event.target)
-    }
+//     onMousemove(event) {
+//         // console.log(event.target)
+//     }
 
-    toHTML() {
-        return createTable();
-    }
-}
+//     toHTML() {
+//         return createTable();
+//     }
+// }
